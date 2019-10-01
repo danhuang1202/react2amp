14589a83378b84a2865e007a175fd83f5b08558c
Dan Huang <danhuang@verizonmedia.com>
2019-10-01 23:31:46 +0800

    fix: rename babel plugin name

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import Html from './components/Html'
 import { useAmp } from './utils'
-import babelPluginAmpClassNams from './plugins/babel'
+import babelPluginAmpClassName from './plugins/babel'
 import webpackPluginAmpAssets from './plugins/webpack'
 import expressAmpMiddleware from './plugins/express'
 
 export {
   Html,
   useAmp,
-  babelPluginAmpClassNams,
+  babelPluginAmpClassName,
   webpackPluginAmpAssets,
   expressAmpMiddleware
 }
