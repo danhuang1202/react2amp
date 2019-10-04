b54bee48cbe0e9657110d7c261736a3e9cb2f36e
Dan Huang <danhuang@verizonmedia.com>
2019-10-04 17:26:57 +0800

    feat: update getAmpAsset()

--- src/utils/asset.ts
+++ src/utils/asset.ts
@@ -1,1 +1,10 @@
-export const getAmpAsset = (entry = '', assetJson = {}) => assetJson[entry]
+export function getAmpAsset(entry: string = '', assetFile: string = '') {
+  let assetMap = {}
+  try {
+    assetMap = require(assetFile) || {}
+  } catch (e) {
+    console.error(`require ${assetFile} failed, ${e}`)
+  }
+
+  return assetMap[entry]
+}
