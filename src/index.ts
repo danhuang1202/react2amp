4a3644ce08e13fe4404a8d27ea53110ce19fb8fb
Dan Huang <danhuang@verizonmedia.com>
2019-10-08 09:42:56 +0800

    fix: update import path

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Html from './components/Html'
 import AmpProvider from './components/AmpProvider'
-import AmpState from './amp/AmpState'
-import AmpAnimation from './amp/AmpAnimation'
+import AmpState from './plugins/AmpState'
+import AmpAnimation from './components/AmpAnimation'
 import { useAmp } from './utils/amp'
 
 export { Html, AmpProvider, AmpState, AmpAnimation, useAmp }
