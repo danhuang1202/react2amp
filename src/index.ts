452a12e688415680cc809c3030cf019f6fc79b74
Dan Huang <danhuang@verizonmedia.com>
2019-10-14 14:00:10 +0800

    feat: remove AmpScript, AmpAnimation and AmpState

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import Html from './components/Html'
 import AmpProvider from './components/AmpProvider'
-import AmpState from './components/AmpState'
-import AmpAnimation from './components/AmpAnimation'
-import AmpScript from './components/AmpScript'
 import { useAmp } from './utils/amp'
 
-export { Html, AmpProvider, AmpState, AmpAnimation, AmpScript, useAmp }
+export { Html, AmpProvider, useAmp }
